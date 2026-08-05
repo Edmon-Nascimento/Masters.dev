59b877c421e36cba0a8a175b1188d148b66f32a2
edmon-nascimento <edmoncode7@gmail.com>
2026-08-05 16:35:25 -0300

    refactor

--- Back-end Path/Java/Classes/src/main/java/challenges/AnnalynsInfiltration.java
+++ Back-end Path/Java/Classes/src/main/java/challenges/AnnalynsInfiltration.java
@@ -31,11 +31,8 @@ public class AnnalynsInfiltration {
     }
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
-        if(petDogIsPresent){
-            return !archerIsAwake;
-        }else{
-            return prisonerIsAwake && !knightIsAwake && !archerIsAwake;
-        }
+            return petDogIsPresent && !archerIsAwake || !petDogIsPresent && prisonerIsAwake && !knightIsAwake && !archerIsAwake;
+
     }
 }
 
