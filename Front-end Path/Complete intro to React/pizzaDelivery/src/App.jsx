2661286b029e8490f58aa661fc01dc808f5a6316
edmon-nascimento <edmoncode7@gmail.com>
2026-09-13 12:42:19 -0300

    add static assets

--- Front-end Path/Complete intro to React/pizzaDelivery/src/App.jsx
+++ Front-end Path/Complete intro to React/pizzaDelivery/src/App.jsx
@@ -1,11 +1,27 @@
 import "./App.css";
 import Pizza from "./components/Pizza";
 
-function App(){
-  return(
+function App() {
+  return (
     <>
+      <h1>Padre Gino's</h1>
+      <Pizza
+        name="Pepperoni"
+        description="pep, cheese, n stuff"
+        image={"/public/pizzas/pepperoni.webp"}
+      />
+      <Pizza
+        name="Pepperoni"
+        description="pep, cheese, n stuff"
+        image={"/public/pizzas/pepperoni.webp"}
+      />
+      <Pizza
+        name="Pepperoni"
+        description="pep, cheese, n stuff"
+        image={"/public/pizzas/pepperoni.webp"}
+      />
     </>
-  )
+  );
 }
 
 export default App;
